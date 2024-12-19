4aeb9766bcd601bc84a7ec5e1b9423f5cf562269
stevvanden <steven.vandenbosch@soprasteria.com>
2024-12-19 14:48:05 +0100

    rename score variables for clarity

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -1,8 +1,8 @@
 
 public class TennisGame3 implements TennisGame {
     
-    private int p2;
-    private int p1;
+    private int player2Score;
+    private int player1Score;
     private final String player1Name;
     private final String player2Name;
 
@@ -13,15 +13,15 @@ public class TennisGame3 implements TennisGame {
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
+        if (player1Score < 4 && player2Score < 4 && !(player1Score + player2Score == 6)) {
             String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+            s = p[player1Score];
+            return (player1Score == player2Score) ? s + "-All" : s + "-" + p[player2Score];
         } else {
-            if (p1 == p2)
+            if (player1Score == player2Score)
                 return "Deuce";
-            s = p1 > p2 ? player1Name : player2Name;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            s = player1Score > player2Score ? player1Name : player2Name;
+            return ((player1Score - player2Score)*(player1Score - player2Score) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
     
@@ -31,9 +31,9 @@ public class TennisGame3 implements TennisGame {
         }
 
         if (player1Name.equals(playerName)) {
-            this.p1 += 1;
+            this.player1Score += 1;
         } else {
-            this.p2 += 1;
+            this.player2Score += 1;
         }
 
     }

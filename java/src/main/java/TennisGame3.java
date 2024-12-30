627fd545e7917e154c2a5e9f7f6ac049e6a57721
stevvanden <steven.vandenbosch@soprasteria.com>
2024-12-30 14:11:07 +0100

    better split cases

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -17,23 +17,23 @@ public class TennisGame3 implements TennisGame {
             String score1Name = scoreNames[player1Score];
             String score2Name = scoreNames[player2Score];
             return (player1Score == player2Score) ? score1Name + "-All" : score1Name + "-" + score2Name;
-        } else {
-            if (player1Score == player2Score)
-                return "Deuce";
-            String playerName = player1Score > player2Score ? player1Name : player2Name;
-            return ((player1Score - player2Score)*(player1Score - player2Score) == 1) ? "Advantage " + playerName : "Win for " + playerName;
+        } else if (player1Score == player2Score) {
+            return "Deuce";
         }
+
+        String playerName = player1Score > player2Score ? player1Name : player2Name;
+        return ((player1Score - player2Score)*(player1Score - player2Score) == 1) ? "Advantage " + playerName : "Win for " + playerName;
     }
-    
+
     public void wonPoint(String playerName) {
         if (!isExistingPlayer(playerName)) {
             throw new IllegalArgumentException("Invalid player name");
         }
 
         if (player1Name.equals(playerName)) {
-            this.player1Score += 1;
+            player1Score += 1;
         } else {
-            this.player2Score += 1;
+            player2Score += 1;
         }
 
     }
